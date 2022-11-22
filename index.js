f2801112e75a2d4173ec290ecc6603cf043d9086
Shadiya Egal <shadiya325@gmail.com>
2022-11-22 09:35:05 -0500

    commit tree f90b482ee92eb962b2e55fb8514b7789bd34e841

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction() {
+    const Spy = chai.spy
+    return spy
+}
+receivesAFunction()
+var fn = returnsANamedFunction
+returnsANamedFunction() 
+function returnsAnAnonymousFunction(){
+
+}
